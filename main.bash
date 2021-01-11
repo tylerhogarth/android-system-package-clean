9d565d7b2c46df153dc52ef1369f2e308d1daba9
Tyler Hogarth <tylerhogarth@gmail.com>
2021-01-11 19:10:19 +0200

    Included params to differenciate facebook and instragram or all

--- main.bash
+++ main.bash
@@ -5,22 +5,54 @@
 ## List all packages: adb shell cmd package list packages
 ## Uninstall package: adb uninstall --user 0 <package-name>
 
-packageArray=(
+facebookPackages=(
   "com.facebook.orca" # Facebook Messenger
   "com.facebook.katana" # Facebook Android App
   "com.example.facebook"
   "com.facebook.android"
+  "com.facebook.services"
+  "com.facebook.system"
+  "com.facebook.appmanager"
+)
+
+instagramPackages=(
   "com.instagram.android"
 )
 
+case $1 in
+
+  "all")
+    packageArray=($(echo ${facebookPackages[*]}) $(echo ${instagramPackages[*]}))
+    ;;
+
+  "facebook")
+    packageArray=($(echo ${facebookPackages[*]}))
+    ;;
+
+  "instagram")
+    packageArray=($(echo ${instagramPackages[*]}))
+    ;;
+
+  *)
+    packageArray=()
+    ;;
+esac
+
+if [ ${#packageArray[@]} -eq 0 ]; then
+    echo "No params received. Provide: all, facebook or instagram"
+    exit
+fi
+
 cmdDevices=$(adb devices)
-echo -e "$cmdDevices"
+echo "$cmdDevices"
+echo
 
 for item in ${packageArray[@]}
 do
   echo "Uninstall : $item"
   adb uninstall --user 0 $item
-  echo -e "$cmdUninstall\n"
+  echo "$cmdUninstall"
+  echo
 done
 
 echo "-- Done --"
